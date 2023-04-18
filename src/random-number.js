38927a8fa944f746b3d71fc385e6fa9f68872482
rampush <arsogarkov@gmail.com>
2023-04-18 13:20:33 +0300

    fix: removed lint unnessesary rule and corrected randomNUM style

--- src/random-number.js
+++ src/random-number.js
@@ -1,7 +1,3 @@
-const randomNum = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default randomNum;
